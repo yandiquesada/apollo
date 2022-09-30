6cd45205c752f4887b856d6b64e35790d99a21be
Yandi Quesada <>
2022-09-29 23:35:28 -0400

    Linking the DrawerLayout and toolbar toggle together.

--- app/src/main/java/com/apollo/MainActivity.kt
+++ app/src/main/java/com/apollo/MainActivity.kt
@@ -2,13 +2,16 @@ package com.apollo
 
 import android.content.Context
 import android.content.Intent
+import android.content.res.Configuration
 import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.PersistableBundle
 import android.util.AttributeSet
 import android.view.Menu
 import android.view.MenuItem
 import android.view.View
+import androidx.appcompat.app.ActionBarDrawerToggle
 import androidx.appcompat.widget.AppCompatButton
 import androidx.core.view.GravityCompat
 import com.apollo.databinding.ActivityMainBinding
@@ -17,7 +20,7 @@ import com.product.presentation.list.ProductListActivity
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
-
+    private lateinit var drawerToggle: ActionBarDrawerToggle
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -36,7 +39,25 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun setupViews() {
+        drawerToggle = ActionBarDrawerToggle(this, binding.drawerLayout, binding.toolbar.toolbar, R.string.drawer_open, R.string.drawer_close)
+        // Setup toggle to display hamburger icon with nice animation
+        drawerToggle.isDrawerIndicatorEnabled = true
+        drawerToggle.syncState()
 
+        // Tie DrawerLayout events to the ActionBarToggle
+        binding.drawerLayout.addDrawerListener(drawerToggle)
+    }
+
+    // needed for hamburger animation
+    override fun onPostCreate(savedInstanceState: Bundle?, persistentState: PersistableBundle?) {
+        super.onPostCreate(savedInstanceState, persistentState)
+        drawerToggle.syncState()
+    }
+
+    // needed for hamburger animation
+    override fun onConfigurationChanged(newConfig: Configuration) {
+        super.onConfigurationChanged(newConfig)
+        drawerToggle.onConfigurationChanged(newConfig)
     }
 
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
@@ -46,28 +67,29 @@ class MainActivity : AppCompatActivity() {
 
     // handle actions in the toolbar = app bar = action bar
     override fun onOptionsItemSelected(item: MenuItem) = when (item.itemId) {
-        // The action bar home/up action should open or close the drawer.
-        android.R.id.home -> {
-            binding.drawerLayout.openDrawer(GravityCompat.START)
-            true
-        }
         R.id.action_products -> {
             launchProductList()
             true
         }
         else -> {
-            super.onOptionsItemSelected(item)
+            // handle action over the hamburger, open the drawer navigation view
+            if (drawerToggle.onOptionsItemSelected(item)) {
+                true
+            } else {
+                super.onOptionsItemSelected(item)
+            }
         }
     }
 
     private fun setUpDrawerContent(navigationView: NavigationView) {
         navigationView.setNavigationItemSelectedListener { menuItem ->
             onDrawerItemSelected(menuItem)
+            // deselect the item by returning false
             false
         }
     }
 
-    // handle actions in the drawer menu items
+    // handle actions in the drawer navigation view menu items
     private fun onDrawerItemSelected(menuItem: MenuItem) {
         when (menuItem.itemId) {
             R.id.menu_item_products -> {
